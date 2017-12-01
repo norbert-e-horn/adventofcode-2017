530dbd2d900423a652ded753594b39ed10703eac
Norbert E. Horn <34156286+norbert-e-horn@users.noreply.github.com>
2017-12-01 14:18:51 +0100

    update python day1

---
+++ day1.py
@@ -0,0 +1,5 @@
+import sys
+l = sys.argv[1]
+n = len(l)
+print(sum([int(l[i]) for i in range(0,n) if l[i]==l[(i+1)%n]]))
+print(sum([int(l[i]) for i in range(0,n) if l[i]==l[(i+n/2)%n]]))
--- day1/tag.py
+++
@@ -1,4 +0,0 @@
-f = open("input.txt")
-l = f.read()
-print( sum([int(l[i]) for i in range (0,len(l)) if l[i]==l[(i+1)%len(l)]]))
-print( sum([int(l[i]) for i in range (0,len(l)) if l[i]==l[(i+len(l)/2)%len(l)]]))
