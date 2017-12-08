454a9f2011615cc477d091f334e6dc32feb09968
Norbert E. Horn <34156286+norbert-e-horn@users.noreply.github.com>
2017-12-08 10:41:09 +0100

    Update day8.py

--- day8/day8.py
+++ day8/day8.py
@@ -1,14 +1,14 @@
 import sys
 d={}
-z={">":lambda x,y:x>y,"<":lambda x,y:x<y,">=":lambda x,y:x>=y,"<=":lambda x,y:x<=y,"==":lambda x,y:x==y,"!=":lambda x,y:x!=y,}
+z={">":lambda x,y:x>y,"<":lambda x,y:x<y,">=":lambda x,y:x>=y,"<=":lambda x,y:x<=y,"==":lambda x,y:x==y,"!=":lambda x,y:x!=y}
 m=0
 for n in sys.argv[1].split("\n"):
-	c = n.split()
+	c=n.split()
 	if not c[0]in d:d[c[0]]=0
 	if not c[4]in d:d[c[4]]=0
 	if z[c[5]](d[c[4]],int(c[6])):
 		if c[1]=="inc":d[c[0]]+=int(c[2])
 		else:d[c[0]]-=int(c[2])
-		if d[c[0]]>m:m = d[c[0]]
+		if d[c[0]]>m:m=d[c[0]]
 print(max(d.values()))
 print(m)
