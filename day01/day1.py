9b30f5d3fc3579fc2689ca0ce5a2baa3aabec6d5
Norbert E. Horn <34156286+norbert-e-horn@users.noreply.github.com>
2017-12-17 17:06:42 +0100

    Update day1.py

--- day01/day1.py
+++ day01/day1.py
@@ -2,4 +2,4 @@ import sys
 l = sys.argv[1]
 n = len(l)
 print(sum([int(l[i]) for i in range(0,n) if l[i]==l[(i+1)%n]]))
-print(sum([int(l[i]) for i in range(0,n) if l[i]==l[(i+n/2)%n]]))
+print(sum([int(l[i]) for i in range(0,n) if l[i]==l[(i+n//2)%n]]))
