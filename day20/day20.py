79a0ec5a4f22059581459f7ee8fd177b93d97873
Norbert E. Horn <34156286+norbert-e-horn@users.noreply.github.com>
2017-12-20 15:20:28 +0100

    Update day20.py

--- day20/day20.py
+++ day20/day20.py
@@ -1,20 +1,14 @@
 import sys,re,itertools
 l=sys.argv[1].split("\n")
-k=[]
-for  j in range(len(l)):
-	i=l[j]
-	s= re.findall("<(.*?)>",i)
-	k.append([list(map(int,y.split(",")))for y in s]+[j])
-d=lambda x:sum([abs(y)for y in x])
-k.sort(key=lambda x:d(x[2]))
+k=[[list(map(int,y.split(",")))for y in re.findall("<(.*?)>",l[j])]+[j] for j in range(len(l))]
+k.sort(key=lambda x:sum([abs(y)for y in x[2]]))
 print(k[0][-1])
-def s(i):
-	for p in[1,0]:
-		for j in [0,1,2]:i[p][j]+=i[p+1][j]
 h=lambda x:x[0]
 c=20
 while c:
-	for i in k:s(i)
+	for i in k:
+		for p in[1,0]:
+			for j in [0,1,2]:i[p][j]+=i[p+1][j]
 	for _,j in itertools.groupby(sorted(k,key=h),h):
 		v=list(j)
 		if len(v)>1:
