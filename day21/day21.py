d1e0cea0c6adcf3a8e5226c89c39a08bfec72472
Norbert E. Horn <34156286+norbert-e-horn@users.noreply.github.com>
2017-12-21 15:09:19 +0100

    Update day21.py

--- day21/day21.py
+++ day21/day21.py
@@ -1,27 +1,19 @@
-import sys
+import sys,itertools
+w=itertools.product
 p={}
 r=range
 for q in sys.argv[1].split("\n"):
 	i,o=q.split(" => ")
 	p[i.replace("/","")]=o.split("/")
 s=[".#.","..#","###"]
-def t(v,k):
-	f=r(v)
-	u=r(v-1,-1,-1)
-	for i in r(8):
-		q=""
-		for j in u if i%2else f:
-			for h in u if i&2else f:q+=k[h][j]if i&4else k[j][h]
-		if q in p:return p[q]
 def b():print(sum(l.count("#")for l in s))
 for x in r(18):
 	n=len(s)
 	v=3if n%2else 2
 	z=n//v
 	q=[""]*(n+z)
-	for i in r(z):
-		for j in r(z):
-			for l in r(v+1):q[i*v+i+l]+=t(v,[s[i*v+l][j*v:j*v+v]for l in r(v)])[l]
+	f=[r(v-1,-1,-1),r(v)]
+	for i,j,l in w(r(z),r(z),r(v+1)):q[i*v+i+l]+=[p[x]for x in["".join(s[i*v+h][j*v+ü]if d&4else s[i*v+ü][j*v+h]for ü,h in w(f[d%2],f[(d&2)>>1]))for d in r(8)]if x in p][0][l]
 	s=q
 	if x==4:b()
 b()
